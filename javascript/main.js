6024bd231b8783d6aaadf4bb2d41ee1f10cd3f3f
Justice Yen <justice.yen@gmail.com>
2017-05-10 22:06:58 -0700

    add arrow and arrow animation

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   const nav = document.getElementById('nav');
+  const pageDown = document.getElementById('pageDown');
   const navTop = document.getElementById('navTop');
   const navMid = document.getElementById('navMid');
   const navBot = document.getElementById('navBot');
+  const arrow = document.getElementById('arrow');
+  const arrowLeft = document.getElementById('arrowLeft');
+  const arrowRight = document.getElementById('arrowRight');
+  const height = window.innerHeight;
   let state = {navOpen: false};
   nav.onclick = () => { //function for turning navbar into cross and back
     if (state.navOpen) {
@@ -17,5 +22,20 @@ document.addEventListener('DOMContentLoaded', function() {
       state.navOpen = true;
     }
   };
+  pageDown.onmouseenter = () => {
+    arrow.className = "arrow arrow-bounce";
+    arrowLeft.className = "arrow-line-left left-arrow-animation";
+    arrowRight.className = "arrow-line-right right-arrow-animation";
+  }
+
+  pageDown.onmouseleave = () => {
+    arrow.className = "arrow";
+    arrowLeft.className = "arrow-line-left";
+    arrowRight.className = "arrow-line-right";
+  }
+
+  pageDown.onclick = () => {
+    window.scrollTo(0, height);
+  }
 
 });
