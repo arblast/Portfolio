1f6a98c68508407ef90bc57be460e5f603ea8495
Justice Yen <justice.yen@gmail.com>
2017-05-17 01:10:34 -0700

    add animations for nav links open close

--- javascript/main.js
+++ javascript/main.js
@@ -4,6 +4,7 @@ const PAGES = {
 
 document.addEventListener('DOMContentLoaded', function() {
   const nav = document.getElementById('nav');
+  const navLinks = document.getElementById('nav-links');
   const pageDown = document.getElementById('pageDown');
   const navTop = document.getElementById('navTop');
   const navMid = document.getElementById('navMid');
@@ -15,15 +16,21 @@ document.addEventListener('DOMContentLoaded', function() {
   let state = {navOpen: false};
   nav.onclick = () => { //function for turning navbar into cross and back
     if (state.navOpen) {
+      nav.className = "square nav";
       navTop.className = "navline top";
       navMid.className = "navline mid";
       navBot.className = "navline bottom";
       state.navOpen = false;
+      navLinks.style.visibility = "hidden";
+      navLinks.style.opacity = 0;
     } else {
+      nav.className = "square nav nav-open"
       navTop.className = "navline top-cross";
       navMid.className = "navline mid-cross";
       navBot.className = "navline bottom-cross";
       state.navOpen = true;
+      navLinks.style.visibility = "visible";
+      navLinks.style.opacity = 100;
     }
   };
   pageDown.onmouseenter = () => {  //scripts for animation of arrow
